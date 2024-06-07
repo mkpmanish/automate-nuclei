23bf1d138dda12cdd23b77a440ff60753d10f7bb
EC2 Default User <ec2-user@ip-172-31-33-248.ap-south-1.compute.internal>
2024-06-07 14:03:28 +0000

    added to git 7308

---
+++ installjenkins.sh
@@ -0,0 +1,19 @@
+sudo yum update –y
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum upgrade
+sudo dnf install java-17-amazon-corretto -y
+sudo yum install java-17-amazon-corretto -y
+sudo yum install jenkins -y
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+sudo yum install docker -y
+sudo yum install git -y
+sudo groupadd docker
+sudo usermod -aG docker $USER
+sudo usermod -aG jenkins $USER
+sudo chmod 666 /var/run/docker.sock
+sudo systemctl restart docker
+sudo systemctl restart jenkins
